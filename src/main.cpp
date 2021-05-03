21d040608571a67090acb16b6f6732f751650642
Lockie Luke <sherlockluk@outlook.com>
2021-05-03 22:43:14 +0800

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <GL/glew.h>
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+#include "renderer/renderer.h"
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Error initing GLFW" << std::endl;
+    }
+
+    glfwSetErrorCallback([](int error, const char *description) {
+        std::cerr << "Error: " << description << std::endl;
+    });
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Zincon", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Error creating window" << std::endl;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(0);
+
+    glfwSetWindowSizeCallback(window, [](GLFWwindow *window, int width, int height){
+        glViewport(0, 0, width, height);
+    });
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        render();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
